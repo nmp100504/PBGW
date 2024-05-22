026015412585bc08fee9689676697ba152d0e1b2
Nguyen Manh Phu <nmp100504@gmail.com>
2024-05-22 13:55:39 +0700

    setup package

---
+++ src/main/java/com/example/pbgw/PBGW.java
@@ -0,0 +1,13 @@
+package com.example.pbgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PBGW {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PBGW.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pbgw/PBGWTests.java
@@ -0,0 +1,13 @@
+package com.example.pbgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PBGWTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
