077d1c936b2dc6bf19cc6909bbf9a360c671df00
Nguyen Manh Phu <nmp100504@gmail.com>
2024-05-24 11:48:03 +0700

    add UserService and UserServiceImpl

--- src/main/java/com/example/pbgw/service/UserService.java
+++ src/main/java/com/example/pbgw/service/UserService.java
@@ -1,2 +1,10 @@
-package com.example.pbgw.service;public interface UserService {
+package com.example.pbgw.service;
+
+
+import com.example.pbgw.dto.UserRegistrationDto;
+import com.example.pbgw.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+    User save(UserRegistrationDto registrationDto);
 }
