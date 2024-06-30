2a6ee91e7ceb3766a5c6096bd278f087894559ad
Nguyen Manh Phu <nmp100504@gmail.com>
2024-06-30 23:55:25 +0700

    change model 'post'

--- src/main/java/com/example/BuildPC/model/Post.java
+++ src/main/java/com/example/BuildPC/model/Post.java
@@ -2,54 +2,37 @@ package com.example.BuildPC.model;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name ="Post")
-public class Post {
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id")
-    private Integer id;
-
-
-    @Column(name = "post_title",columnDefinition = "TEXT")
+    private long id;
+    @Column(nullable = false)
     private String title;
-
-    @Column(name = "post_content",columnDefinition = "TEXT")
-
+    private String url;
+    @Lob
+    @Column(nullable = false)
     private String content;
+    private String shortDescription;
 
-    @Column(name = "post_create_at")
-    private Date createdAt;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
 
-    @Column(name = "post_update_at")
-    private Date updatedAt;
-
-    @Column(name = "post_image")
-    private String imageFileName;
-
-
-    @ManyToOne()
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    public Post(String postTitle, String postConent, Date postCreatedAt, Date postUpdateAt, User user, String imageFileName) {
-        this.imageFileName = imageFileName;
-        this.title = postTitle;
-        this.content = postConent;
-        this.createdAt = postCreatedAt;
-        this.updatedAt = postUpdateAt;
-        this.user = user;
-    }
-
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
 
 }
