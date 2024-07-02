a21865c559952f38120966564a29b41d540ba215
Nguyen Manh Phu <nmp100504@gmail.com>
2024-07-02 23:44:55 +0700

    refactor PostDTO

--- src/main/java/com/example/BuildPC/dto/PostDto.java
+++ src/main/java/com/example/BuildPC/dto/PostDto.java
@@ -2,53 +2,21 @@ package com.example.BuildPC.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class PostDto {
-
-    @NotEmpty(message = "The title is required")
+    private Long id;
     private String title;
-
-    @Size(min = 10, message = "The content should be at least 10 characters")
-    @Size(max = 500000, message = "The content cannot exceed 500000 characters")
+    private String url;
     private String content;
+    private String shortDescription;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
 
-    private MultipartFile imageFile;
-
-    // Add user information
-    private Long userId; // Assuming user ID is passed from the client
-
-    // Getters and Setters
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public MultipartFile getImageFile() {
-        return imageFile;
-    }
-
-    public void setImageFile(MultipartFile imageFile) {
-        this.imageFile = imageFile;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
 }
