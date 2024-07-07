c9f06fa17ab38253659d25e7df0eaf01149b909b
Nguyen Manh Phu <nmp100504@gmail.com>
2024-07-07 14:45:55 +0700

    create PostMapper class

---
+++ src/main/java/com/example/BuildPC/mapper/PostMapper.java
@@ -0,0 +1,30 @@
+package com.example.BuildPC.mapper;
+
+import com.example.BuildPC.dto.PostDto;
+import com.example.BuildPC.model.Post;
+
+public class PostMapper {
+    public static PostDto mapToPostDTO(Post post) {
+        return PostDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .url(post.getUrl())
+                .shortDescription(post.getShortDescription())
+                .createdOn(post.getCreatedOn())
+                .updatedOn(post.getUpdatedOn())
+                .build();
+    }
+
+    public static Post mapToPost(PostDto postDto) {
+        return Post.builder()
+                .id(postDto.getId())
+                .title(postDto.getTitle())
+                .content(postDto.getContent())
+                .url(postDto.getUrl())
+                .shortDescription(postDto.getShortDescription())
+                .createdOn(postDto.getCreatedOn())
+                .updatedOn(postDto.getUpdatedOn())
+                .build();
+    }
+}
--- src/main/java/com/example/BuildPC/service/PostService.java
+++ src/main/java/com/example/BuildPC/service/PostService.java
@@ -1,5 +1,6 @@
 package com.example.BuildPC.service;
 
+import com.example.BuildPC.dto.PostDTO;
 import com.example.BuildPC.model.Post;
 import com.example.BuildPC.model.User;
 import org.springframework.stereotype.Service;
@@ -9,15 +10,15 @@ import java.util.Optional;
 
 @Service
 public interface PostService {
-    List<Post> findAllPost(Integer pageNo, Integer pageSize, String search);
+    List<PostDTO> findAllPost();
 
-    Post findPostById(Integer postId);
-
-    Post createNewPost(Post post);
-
-    Boolean updatePost(Post post);
-
-    Boolean deletePostById(int id);
-
-    Optional<User> getCurrentUser();
+//    Post findPostById(Integer postId);
+//
+//    Post createNewPost(Post post);
+//
+//    Boolean updatePost(Post post);
+//
+//    Boolean deletePostById(int id);
+//
+//    Optional<User> getCurrentUser();
 }
