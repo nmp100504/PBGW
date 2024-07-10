d30bff639fd6b5dab762ee288c0b44822f731afe
Nguyen Manh Phu <nmp100504@gmail.com>
2024-07-10 09:30:26 +0700

    display list of blog be impl

---
+++ src/main/java/com/example/BuildPC/controller/Marketing/BlogController.java
@@ -0,0 +1,28 @@
+package com.example.BuildPC.controller.Marketing;
+
+import com.example.BuildPC.dto.PostDto;
+import com.example.BuildPC.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BlogController {
+
+    private PostService postService;
+
+    public BlogController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/blog")
+    public String viewBlogPosts(Model model){
+        List<PostDto> postsResponse = postService.findAllPost();
+        model.addAttribute("posts", postsResponse);
+        return "marketing/blog_details";
+    }
+
+
+}
