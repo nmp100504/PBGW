03e04d47f691f987bc385bb9e66ef05997ec28fb
Nguyen Manh Phu <nmp100504@gmail.com>
2024-07-10 10:56:04 +0700

    blog search be impl

--- src/main/java/com/example/BuildPC/controller/Marketing/BlogController.java
+++ src/main/java/com/example/BuildPC/controller/Marketing/BlogController.java
@@ -1,13 +1,18 @@
 package com.example.BuildPC.controller.Marketing;
 
 import com.example.BuildPC.dto.PostDto;
+import com.example.BuildPC.model.Post;
 import com.example.BuildPC.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@RequestMapping("/blog")
 @Controller
 public class BlogController {
 
@@ -17,12 +22,24 @@ public class BlogController {
         this.postService = postService;
     }
 
-    @GetMapping("/blog")
+    @GetMapping("")
     public String viewBlogPosts(Model model){
         List<PostDto> postsResponse = postService.findAllPost();
         model.addAttribute("posts", postsResponse);
         return "marketing/blog";
     }
 
+    @GetMapping("/{postUrl}")
+    private String showBlogPost(@PathVariable("postUrl") String postUrl, Model model){
+        PostDto postDto = postService.findPostByUrl(postUrl);
+        model.addAttribute("post", postDto);
+        return "marketing/view";
+    }
 
+    @GetMapping("/search")
+    public String searchPost(@RequestParam(value = "query") String query, Model model){
+        List<PostDto> postsResponse = postService.searchPosts(query);
+        model.addAttribute("posts", postsResponse);
+        return "marketing/blog";
+    }
 }
