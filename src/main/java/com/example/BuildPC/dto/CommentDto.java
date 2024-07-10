d74b97957ff20d9bc06a807bcde78e79c9daad72
Nguyen Manh Phu <nmp100504@gmail.com>
2024-07-10 11:29:19 +0700

    create comment dto

---
+++ src/main/java/com/example/BuildPC/dto/CommentDto.java
@@ -0,0 +1,23 @@
+package com.example.BuildPC.dto;
+
+import com.example.BuildPC.model.Post;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String content;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
