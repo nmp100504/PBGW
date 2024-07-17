847d916fb806017da022a11cd0e9ca6133e6fbde
Nguyen Manh Phu <nmp100504@gmail.com>
2024-07-17 09:58:24 +0700

    fix shop_details th:action for form submitted

--- src/main/java/com/example/BuildPC/controller/ShoppingCartController.java
+++ src/main/java/com/example/BuildPC/controller/ShoppingCartController.java
@@ -4,6 +4,7 @@ import com.example.BuildPC.configuration.CustomUserDetails;
 import com.example.BuildPC.model.*;
 import com.example.BuildPC.service.*;
 import com.example.BuildPC.service.implementation.UserServiceImpl;
+import com.example.BuildPC.utils.SecurityUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -32,9 +34,9 @@ public class ShoppingCartController {
 
 
     @GetMapping("/cart")
-    public String showShoppingCart(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        if (userDetails != null) {
-            Optional<User> currentUserOpt = userService.findByEmail(userDetails.getEmail());
+    public String showShoppingCart(Model model) {
+        String email = Objects.requireNonNull(SecurityUtils.getCurrentUser()).getEmail();
+        Optional<User> currentUserOpt = userService.findByEmail(email);
             if (currentUserOpt.isPresent()) {
                 User user = currentUserOpt.get();
                 List<CartItem> cartItems = shoppingCartService.listCartItems(user);
@@ -48,7 +50,7 @@ public class ShoppingCartController {
                 model.addAttribute("totalPrice", totalPrice);
                 return "LandingPage/cart";
             }
-        }
+
         return "auth/login_page";
     }
 
@@ -56,27 +58,23 @@ public class ShoppingCartController {
     public String addItemToCart(
             @RequestParam("id") Integer productId,
             @RequestParam(value = "quantity", required = false, defaultValue = "1") Integer quantity,
-            @AuthenticationPrincipal CustomUserDetails userDetails,
             HttpServletRequest request) {
-
-        if( userDetails != null) {
-            Optional<User> user = userService.findByEmail(userDetails.getEmail());
+        String email = Objects.requireNonNull(SecurityUtils.getCurrentUser()).getEmail();
+        Optional<User> user = userService.findByEmail(email);
             if (user.isPresent()) {
                 shoppingCartService.addProduct(productId, quantity, user.get());
                 return "redirect:" + request.getHeader("referer");
             }
-        }
         return "redirect:/login";
     }
 
 
     @PostMapping("/update-cart")
     public String updateCart(@RequestParam Map<String, String> allParams,
-                             @AuthenticationPrincipal CustomUserDetails userDetails,
                              HttpServletRequest request) {
-        if (userDetails != null) {
-            Optional<User> user = userService.findByEmail(userDetails.getEmail());
-            if (user.isPresent()) {
+        String email = Objects.requireNonNull(SecurityUtils.getCurrentUser()).getEmail();
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isPresent()) {
                 for (Map.Entry<String, String> entry : allParams.entrySet()) {
                     if (entry.getKey().startsWith("quantities[")) {
                         Integer productId = Integer.valueOf(entry.getKey().substring(11, entry.getKey().length() - 1));
@@ -89,21 +87,19 @@ public class ShoppingCartController {
                 }
                 return "redirect:" + request.getHeader("referer");
             }
-        }
+
         return "redirect:/login";
     }
 
     @PostMapping("/delete-cart-item")
     public String deleteCartItem(@RequestParam("productId") Integer productId,
-                                 @AuthenticationPrincipal CustomUserDetails userDetails,
                                  HttpServletRequest request) {
-        if (userDetails != null) {
-            Optional<User> user = userService.findByEmail(userDetails.getEmail());
-            if (user.isPresent()) {
+        String email = Objects.requireNonNull(SecurityUtils.getCurrentUser()).getEmail();
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isPresent()) {
                 shoppingCartService.deleteProduct(productId, user.get());
                 return "redirect:" + request.getHeader("referer");
             }
-        }
         return "redirect:/login";
     }
 }
